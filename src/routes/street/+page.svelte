4701b14a2289203b37633eca624415b54dc20cca
pedropark99 <pedropark99@gmail.com>
2024-04-13 16:18:59 -0300

    Now we have an image list

--- src/routes/street/+page.svelte
+++ src/routes/street/+page.svelte
@@ -1,12 +1,28 @@
 <script>
     import MainMenu from "../mainMenu.svelte";
-    import ImageList from "../imageList.svelte";
+
+	const images = [
+		'IMG_8363.jpg',
+		'IMG_8378.jpg',
+		'IMG_8383.jpg'
+	]
 </script>
 
 
 <div class="window">
 	<MainMenu/>
 	<div class="pageContent">
-		<ImageList/>
+		<div class='imageList'>
+			{#each images as image}
+				<img src="{image}" alt="" width="60%" />
+			{/each}
+		</div>
 	</div>
 </div>
+
+
+<style>
+	img {
+		border-radius: 15px;
+	}
+</style>
