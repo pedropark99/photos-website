0d533767bfe6ed85e9c4728c4904dd6aaea2d58a
pedropark99 <pedropark99@gmail.com>
2024-04-17 19:09:40 -0300

    Add switch pure css cards

--- src/routes/cards.svelte
+++ src/routes/cards.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { onMount } from 'svelte';
     const images = [
         'street/IMG_8513.jpg',
         'street/IMG_8383.jpg',
@@ -7,6 +8,12 @@
     
     var current_image_index = 0
     let current_image = images[0]
+    onMount(() => {
+        var buttons = document.getElementsByClassName("buttonSwitchCard");
+        buttons[current_image_index].focus();
+    });
+
+
     function next_image() {
         if (current_image_index === images.length) {
             // Do nothing
@@ -14,8 +21,11 @@
         }
         
         current_image_index++
-        current_image=images[current_image_index];
+        current_image = images[current_image_index];
+        var buttons = document.getElementsByClassName("buttonSwitchCard");
+        buttons[current_image_index].focus();
     }
+
     function previous_image() {
         if (current_image_index === 0) {
             // Do nothing
@@ -23,7 +33,18 @@
         }
         
         current_image_index--
-        current_image=images[current_image_index];
+        current_image = images[current_image_index];
+        var buttons = document.getElementsByClassName("buttonSwitchCard");
+        buttons[current_image_index].focus();
+    }
+
+    function switch_image_focus(button_elem) {
+        var buttons = document.getElementsByClassName("buttonSwitchCard");
+        var index_as_int = parseInt(button_elem.target.id);
+        buttons[index_as_int].focus();
+
+        current_image_index = index_as_int;
+        current_image = images[current_image_index];
     }
 </script>
 
@@ -34,8 +55,13 @@
     </div>
 
     <div class="buttonsContainer">
-        <button class="buttonSwitchCard" on:click={previous_image}>←</button>
-        <button class="buttonSwitchCard" on:click={previous_image}>→</button>
+
+            <button class="arrowSwitchCard" on:click={previous_image}>←</button>
+            {#each images as image, index }
+                <button id="{index}" class="buttonSwitchCard" on:click={(event) => switch_image_focus(event)}></button>
+            {/each}
+            <button class="arrowSwitchCard" on:click={next_image}>→</button>
+        
     </div>
 </div>
 
@@ -45,13 +71,60 @@
 		display: grid;
 		grid-template-rows: 85% 15%;
         grid-template-columns: 1fr;
+        padding-top: 20px;
+        gap: 10px;
 		place-items: center;
 		text-align: center;
 	}
-	.buttonSwitchCard {
-		color: var(--main-text-brown-color);
-        font-size: 25pt;
+
+    .cardsContainer img {
+        width: auto;
+        height: 500px;
+    }
+
+	.arrowSwitchCard {
+		color: var(--argentinian-blue);
+        font-size: 28pt;
         background: none;
         border: none;
+        margin-left: 20px;
+        margin-right: 20px;
 	}
+
+    .buttonsContainer {
+        display: flex;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        list-style: none;
+        margin: 0;
+        border: 0;
+    }
+
+    .buttonSwitchCard {
+        height: 15px;
+        width: 15px;
+        background-color: #adadad;
+        border: none;
+        border-radius: 50%;
+        display: inline-block;
+        margin: 5px;
+    }
+
+    .buttonSwitchCard:active {
+        background-color: var(--argentinian-blue);
+        border: #222222;
+    }
+
+    .buttonSwitchCard:focus {
+        background-color: var(--argentinian-blue);
+        border: #222222;
+    }
+
+    .arrowSwitchCard:hover {
+        background-color: var(--main-text-brown-color);
+        border-radius: 5px;
+	}
+
+
 </style>
