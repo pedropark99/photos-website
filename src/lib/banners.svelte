459362032a9c965dd2952b4ee2a7485003f644cc
pedropark99 <pedropark99@gmail.com>
2025-02-21 09:04:16 -0300

    Add banners layout

--- src/lib/banners.svelte
+++ src/lib/banners.svelte
@@ -1,22 +1,63 @@
 <script>
     const banner_img1 = "events/ensaio-rojo-2025/IMG_2770.webp";
+    const banner_img2 = "events/galinheiro-rep-14anos/IMG_0479.webp";
+    const banner_img3 = "IMG_3378.webp";
 </script>
 
-
+<div class="bannerMargin"/>
 <div id="banner1" class="homepageBanner">
     <div class="bannerImageContainer">
         <img alt="" src="{banner_img1}" />
     </div>
+
+    <div id="banner1" class="textInBanner">
+        <h1>Retratos</h1>
+        <p>
+            Guarde recordações sobre grandes momentos de sua vida
+            (casamento, festa de 15 anos, formaturas) com um
+            ensaio pessoal e personalizado.
+        </p>
+    </div>
 </div>
+
+<div id="banner2" class="homepageBanner">
+    <div id="banner2" class="textInBanner">
+        <h1>Cobertura de eventos</h1>
+        <p>
+            Uma boa forma de se posicionar no mercado é ter uma fotografia
+            que comunique quem você é para os seus clientes.
+        </p>
+    </div>
+
+    <div id="banner2" class="bannerImageContainer">
+        <img alt="" src="{banner_img2}" />
+    </div>
+</div>
+
+<div id="banner1" class="homepageBanner">
+    <div id="banner1" class="bannerImageContainer">
+        <img alt="" src="{banner_img3}" />
+    </div>
+
+    <div id="banner1" class="textInBanner">
+        <h1>Ensaios corporativos</h1>
+        <p>
+            Uma boa forma de se posicionar no mercado é ter uma fotografia
+            que comunique quem você é para os seus clientes.
+        </p>
+    </div>
+</div>
+
+<div class="bannerMargin"/>
+
 
 <style>
     .homepageBanner {
         display: flex;
+        justify-content: center;
         align-items: center;
         height: 500px;
         width: 100vw;
-        margin-top: 30px;
-        margin-bottom: 30px;
     }
 
     #banner1 {
@@ -26,6 +67,36 @@
     .bannerImageContainer {
         height: 80%;
         padding: 40px;
+    }
+
+    .homepageBanner h1 {
+        font-family: Outfit, serif;
+		font-size: 40pt;
+		font-weight: bold;
+    }
+
+    #banner1.textInBanner h1 {
+        color: var(--moonstone-blue);
+    }
+
+    #banner1.textInBanner p {
+        color: white;
+    }
+
+    #banner2.textInBanner h1 {
+        color: var(--main-text-brown-color);
+    }
+
+    #banner2.textInBanner p {
+        color: black;
+    }
+
+    .textInBanner {
+        max-width: 25rem;
+    }
+
+    .bannerMargin {
+        height: 40px;
     }
 
     img {
